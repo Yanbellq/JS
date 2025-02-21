a3e93a2a7add055ce98a6ac8a23ba3b17a59ed30
Yanbellq <yanbellq@gmail.com>
2025-02-22 00:52:03 +0200

    Project: Lab3 = task3 done, wip;

--- lab3/js/app.js
+++ lab3/js/app.js
@@ -34,5 +34,82 @@ document.getElementById('form').addEventListener('submit', function (event) {
     }
 
 
-    document.getElementById('num').value = '';
+    document.getElementById('num').value = '';  
 });
+
+// Task - 3
+const showMonth = () => {
+    console.log("\n\nTask - 3\n\n");
+    
+    let num = parseInt(prompt("Введіть число (1 - 12):"));
+
+    switch (num)
+    {
+        case 1:
+        {
+            console.log("Month: January");
+            break;
+        }
+        case 2:
+        {
+            console.log("Month: February");
+            break;
+        }
+        case 3:
+        {
+            console.log("Month: March");
+            break;
+        }
+        case 4:
+        {
+            console.log("Month: April");
+            break;
+        }
+        case 5:
+        {
+            console.log("Month: May");
+            break;
+        }
+        case 6:
+        {
+            console.log("Month: June");
+            break;
+        }
+        case 7:
+        {
+            console.log("Month: July");
+            break;
+        }
+        case 8:
+        {
+            console.log("Month: August");
+            break;
+        }
+        case 9:
+        {
+            console.log("Month: September");
+            break;
+        }
+        case 10:
+        {
+            console.log("Month: October");
+            break;
+        }
+        case 11:
+        {
+            console.log("Month: November");
+            break;
+        }
+        case 12:
+        {
+            console.log("Month: December");
+            break;
+        }
+        default:
+        {
+            console.log("Enter the correct number");
+            break;
+        }
+    }
+}
+
