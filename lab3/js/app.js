71de625ff008529dc30bcd306d89c991e545e797
Yanbellq <yanbellq@gmail.com>
2025-02-22 00:25:13 +0200

    Project: Lab3 - task2 done, wip;

--- lab3/js/app.js
+++ lab3/js/app.js
@@ -8,3 +8,31 @@ while (n < 50)
 }
 
 console.log(n);
+
+// Task - 2
+console.log("\n\nTask - 2\n\n");
+document.getElementById('form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    let num = document.getElementById('num').value;
+
+    const faktorial = (num) => {
+        if (num < 0) {
+            return -1;
+        } 
+        let result = 1;
+        for (let i = 2; i <= num; i++) {
+            result *= i;
+        }
+
+        return result;
+    }
+
+    if (!isNaN(num)) {
+        console.log(`Факторіал числа ${num} = ${faktorial(num)}`);
+    } else {
+        console.log("Будь ласка, введіть коректне число.");
+    }
+
+
+    document.getElementById('num').value = '';
+});
