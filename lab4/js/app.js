c6ad6349f29a78a1f5995ddfc1d5179354a7250b
Yanbellq <yanbellq@gmail.com>
2025-03-10 23:01:42 +0200

    Project: Lab4 - task2 done, wip

--- lab4/js/app.js
+++ lab4/js/app.js
@@ -1,21 +1,39 @@
-// Task - 1
+let array;
+
+const task1 = (array) => { 
+    console.log(array.toString().concat(' - Початковий масив'));
+
+    array.pop()
+    console.log(array.toString().concat(' - Без останнього елемента'));
+
+    array.unshift("ананас");
+    console.log(array.toString().concat(' - З новим першим елементом'));
+
+    array.sort().reverse();
+    console.log(array.toString().concat(' - У зворотньому алфавітному порядку'));
+
+    console.log(array.indexOf('яблуко').toString().concat(' - Індекс яблука'));
+}
 
 console.log('Task - 1\n\n');
-let fruits = ["яблуко", "банан", "апельсин", "груша", "виноград"];
+task1(array = ["яблуко", "банан", "апельсин", "груша", "виноград"]);
 
-console.log(fruits.toString().concat(' - Початковий масив'));
+// --------------------------------------------------------------------------------------------
 
-fruits.pop()
-console.log(fruits.toString().concat(' - Без останнього елемента'));
+const task2 = (array) => { 
+    console.log(array.toString().concat(' - Початковий масив'));
 
-fruits.unshift("ананас");
-console.log(fruits.toString().concat(' - З новим першим елементом'));
+    let longest = array.reduce((a, b) => a.length > b.length ? a : b);
+    let shortest = array.reduce((a, b) => a.length < b.length ? a : b);
+    console.log(longest.concat(' - Найдовший елемент'));
+    console.log(shortest.concat(' - Найкоротший елемент'));
 
-fruits.sort().reverse();
-console.log(fruits.toString().concat(' - У зворотньому алфавітному порядку'));
+    let array1 = array.filter(item => item.includes("синій"));
+    console.log(array1.toString().concat(' - Тільки з елементами, що містять "синій"'));
 
-console.log(fruits.indexOf('яблуко').toString().concat(' - Індекс яблука'));
+    let joinedString = array.join(', ');
+    console.log(joinedString.concat(' - Об\'єднаний рядок'));
+}
 
-
-// Task - 2
 console.log('\nTask - 2\n\n');
+task2(array = ["червоний", "синій", "зелений", "жовтий", "фіолетовий", "блакитний", "оранжевий", "рожевий", "білий", "синій"]);
