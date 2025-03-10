5b7155c674c2c04b58b24852db91485e4c71c109
Yanbellq <yanbellq@gmail.com>
2025-03-10 23:15:03 +0200

    Project: Lab4 - task3 done, wip

--- lab4/js/app.js
+++ lab4/js/app.js
@@ -37,3 +37,47 @@ const task2 = (array) => {
 
 console.log('\nTask - 2\n\n');
 task2(array = ["червоний", "синій", "зелений", "жовтий", "фіолетовий", "блакитний", "оранжевий", "рожевий", "білий", "синій"]);
+
+// --------------------------------------------------------------------------------------------
+
+const task3 = () => {
+    let employees = [
+        { name: "Олександр", age: 30, position: "розробник" },
+        { name: "Марія", age: 25, position: "дизайнер" },
+        { name: "Іван", age: 35, position: "тестувальник" },
+        { name: "Олена", age: 28, position: "розробник" },
+        { name: "Петро", age: 40, position: "менеджер" }
+    ];
+
+    console.log(employees.map(e => e.name).toString().concat(' - Початковий масив'));
+
+    employees.sort((a, b) => a.name.localeCompare(b.name));
+    console.log(employees.map(e => e.name).toString().concat(' - Відсортований масив'));
+
+    let developers = employees.filter(employee => employee.position === "розробник");
+    console.log(developers.map(e => e.name).toString().concat(' - Розробники'));
+
+    employees = employees.filter(employee => employee.age < 40);
+    console.log(employees.map(e => e.name).toString().concat(' - Без працівників старше 40 років'));
+
+    employees.push({ name: "Андрій", age: 22, position: "стажер" });
+    console.log(employees.map(e => e.name).toString().concat(' - Оновлений масив'));
+}
+
+console.log('\nTask - 3\n\n');
+task3();
+
+// --------------------------------------------------------------------------------------------
+
+const task4 = () => { 
+    let students = [
+        { name: "Олександр", age: 24, course: 6 },
+        { name: "Максим", age: 19, course: 2 },
+        { name: "Ольга", age: 17, course: 1 },
+        { name: "Владислав", age: 20, course: 3 },
+        { name: "Данило", age: 19, course: 2 }
+    ]
+}
+
+console.log('\nTask - 4\n\n');
+task4();
