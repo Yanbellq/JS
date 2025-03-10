c208626e908b793c546bafb927baefb289ac6f72
Yanbellq <yanbellq@gmail.com>
2025-03-10 23:50:07 +0200

    Project: Lab4 - task6 done, wip

--- lab4/js/app.js
+++ lab4/js/app.js
@@ -122,3 +122,68 @@ const task5 = () => {
 
 console.log('\nTask - 5\n\n');
 task5();
+
+// --------------------------------------------------------------------------------------------
+
+const libraryManagement = () => {
+    let books = [
+        { title: "Книга 1", author: "Автор 1", genre: "Жанр 1", pages: 200, isAvailable: true },
+        { title: "Книга 2", author: "Автор 2", genre: "Жанр 2", pages: 150, isAvailable: false },
+        { title: "Книга 3", author: "Автор 1", genre: "Жанр 3", pages: 300, isAvailable: true }
+    ];
+
+    const addBook = (title, author, genre, pages) => {
+        books.push({ title, author, genre, pages, isAvailable: true });
+    };
+
+    const removeBook = (title) => {
+        books = books.filter(book => book.title !== title);
+    };
+
+    const findBooksByAuthor = (author) => {
+        return books.filter(book => book.author === author);
+    };
+
+    const toggleBookAvailability = (title, isBorrowed) => {
+        let book = books.find(book => book.title === title);
+        if (book) {
+            book.isAvailable = !isBorrowed;
+        }
+    };
+
+    const sortBooksByPages = () => {
+        books.sort((a, b) => a.pages - b.pages);
+    };
+
+    const getBooksStatistics = () => {
+        let totalBooks = books.length;
+        let availableBooks = books.filter(book => book.isAvailable).length;
+        let borrowedBooks = totalBooks - availableBooks;
+        let averagePages = books.reduce((acc, book) => acc + book.pages, 0) / totalBooks;
+
+        return `Кількість книжок: ${totalBooks}\nВ наявності: ${availableBooks}\nВзяті: ${borrowedBooks}\nСередня кількість сторінок у книзі: ${averagePages.toFixed(2)}`;
+    };
+
+    return {
+        addBook,
+        removeBook,
+        findBooksByAuthor,
+        toggleBookAvailability,
+        sortBooksByPages,
+        getBooksStatistics
+    };
+};
+
+console.log('\nTask - 6\n\n');
+const library = libraryManagement();
+
+library.addBook("Книга 4", "Автор 3", "Жанр 4", 250);
+library.removeBook("Книга 2");
+console.log(library.findBooksByAuthor("Автор 1").map(book => book.title).toString().concat(' - Книги автора 1'));
+
+library.toggleBookAvailability("Книга 1", true);
+library.sortBooksByPages();
+console.log(library.getBooksStatistics());
+
+// --------------------------------------------------------------------------------------------
+
